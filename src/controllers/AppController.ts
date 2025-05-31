48d0154c9282d7d044775e5ecf80b7cb8546aec8
thedunncodes <tobioluwa348@gmail.com>
2025-05-31 12:12:40 +0100

    UPDATE: brief server status information

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
+import dbClient from '../libs/database/db';
 
 export default class AppController {
-    static home(req: Request, res: Response) {
+    static async home(req: Request, res: Response) {
         res.status(200).json({
             message: "Welcome to RideFleet dealership ",
+            operations: `${dbClient.isAlive() ? "Fully operational" : "Partially operational" }. Check '/stat' endpoint for more details`,
         });
     }
 }
