af189dd6b67260000d866b095795d97452475d81
thedunncodes <tobioluwa348@gmail.com>
2025-06-03 00:08:51 +0100

    UPDATE: data type for updating car information

--- src/constants/carTypes.ts
+++ src/constants/carTypes.ts
@@ -10,3 +10,16 @@ export interface carsData {
     year: number;
     imgUrl: string;    
 }
+
+export interface carsUpdateData {
+    brand?: string;
+    model?: string;
+    bodyType?: string;
+    transmission?: 'manual' | 'automatic';
+    price?: number;
+    horsePower?: number;
+    fuelType?: 'petrol' | 'diesel' | 'electric' | 'hybrid';
+    mileage?: number;
+    year?: number;
+    imgUrl?: string;
+}
