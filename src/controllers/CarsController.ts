658d795a474433c39e122acb9f11720408834c20
thedunncodes <tobioluwa348@gmail.com>
2025-06-03 13:51:56 +0100

    UPDATE: added a 'buyCar' controller to handle car purchases

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
-import { ObjectId } from "mongodb";
+import { ObjectId, WithId, Document } from "mongodb";
 import dbClient from "../libs/database/db";
 import { Request, Response } from "express";
 import { carsData, carsUpdateData } from "../constants/carTypes";
@@ -177,6 +177,99 @@ export default class CarsController {
         
             res.status(200).json({ message: `Car with id '${carId}' deleted successfully` });
             return;
+        }
+
+        res.status(500).json({ error: "Internal server error, visit '/stat' endpoint." });
+        return;
+    }
+
+    static async buyCar(req: Request, res: Response) {
+        const xToken = req.headers['x-token'];
+        const carId = req.params.carId;
+        const { amountPayed }: { amountPayed: number } = req.body || {};
+                
+        if (!xToken) {
+            res.status(401).json({ error: "Unauthorized, no access token provided, procced to '/login' route " });
+            return;
+        }
+        const { validSession, payload } = protectSession(xToken as string);
+        if (!validSession) {
+            res.status(401).json({ error: "Invalid session token, or expired session" });
+            return;
+        }
+
+        if (!carId) {
+            res.status(400).json({ error: "Car ID is required" });
+            return;
+            }
+
+        if (dbClient.isAlive()) {
+            let carInfo: WithId<Document> | null; ;
+            if (!carId) {
+                res.status(400).json({ error: "Car ID is required" });
+                return;
+            } else {
+                const { carPresent, carData } = await checkCarExists(carId);
+                if (!carPresent) {
+                    res.status(404).json({ error: "Car not found/Invalid car id " });
+                    return;
+                }
+                carInfo = carData;
+
+                if (typeof amountPayed !== 'number' || amountPayed <= 0) {
+                    res.status(400).json({ error: "Invalid amount provided, it should be a positive number" });
+                    return;
+                }
+
+                if (carInfo && carInfo.sold) {
+                    res.status(400).json({ error: `Car with id '${carData?._id.toString()}' already sold` });
+                    return;
+                }
+
+                if (carInfo && (carInfo.price > amountPayed)) {
+                    res.status(400).json({ error: `Insufficient amount, the car price is $${carInfo.price}, but you provided $${amountPayed}` });
+                    return;
+                }
+            }
+            
+            const carsColl = dbClient.db?.collection("cars");
+            try {
+                const result = await carsColl?.updateOne(
+                    { _id: new ObjectId(carId) },
+                    { $set: { sold: true } }
+                );
+                if (!result || result.modifiedCount === 0) {
+                    res.status(404).json({ error: "Car not found or already sold" });
+                    return;
+                }
+            } catch {
+                res.status(400).json({ error: "Invalid Car Id" });
+                return;
+            }
+        
+            try {
+                const result = await dbClient.db?.collection("sales").insertOne({
+                    carId: new ObjectId(carId),
+                    buyerId: new ObjectId(payload.id),
+                    brand: carInfo?.brand,
+                    model: carInfo?.model,
+                    price: carInfo?.price,
+                    imgUrl: carInfo?.imgUrl,
+                    amountPayed,
+                    soldAt: DateTime.utc().toISO(),
+                })
+
+                if (result && result.acknowledged) {
+                    res.status(200).json({ message: `Purchase successful with purchase id: ${result.insertedId}` });
+                    return;
+                } else {
+                    res.status(500).json({ error: "Error recording the sale, please try again later." });
+                    return;
+                }
+            } catch {
+                res.status(500).json({ error: "Error recording the sale, please try again later." });
+                return;
+            }
         }
 
         res.status(500).json({ error: "Internal server error, visit '/stat' endpoint." });
