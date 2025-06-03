db7d5d4b2f6303d54c00990bba12fbe595cd7282
thedunncodes <tobioluwa348@gmail.com>
2025-06-03 12:17:12 +0100

    UPDATE: improved 'getCars' controller to sort, filter and paginate queried data

--- src/controllers/AppController.ts
+++ src/controllers/AppController.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express';
 import dbClient from '../libs/database/db';
+import { carQueryData } from '../constants/carTypes';
+import selectedFields from '../utils/queryUtils/selectedFields';
+import getFieldData from '../utils/queryUtils/getFieldData';
+import filterQueryData from '../utils/queryUtils/filterQueryData';
+import protectSession from '../utils/authUtils/protectSession';
+import paginate from '../utils/pagination/paginate';
 
 export default class AppController {
     static async home(req: Request, res: Response) {
@@ -20,13 +26,57 @@ export default class AppController {
     }
 
     static async getCars(req: Request, res: Response) {
-        if (dbClient.isAlive()) {
-            const carsColl = dbClient.db?.collection("cars");
-            const cars = await carsColl?.find({ sold: false }).toArray() || [];
-            res.status(200).json(cars);
+        const xToken = req.headers['x-token'];
+        const {
+            brand, model, bodyType, fuelType,
+            transmission, price, mileage, year, 
+         }: carQueryData = req.query;
+        const { page, size } = req.query;
+
+        const validFields = selectedFields({
+            brand, model, bodyType, fuelType,
+            transmission, price, mileage, year
+        }) 
+
+        const queryData = await getFieldData(
+            validFields, {
+                brand, model, bodyType, fuelType,
+                transmission, price, mileage, year
+            }
+        )
+
+        const { formattedData, filteredData } = filterQueryData(queryData, validFields, {
+            brand, model, bodyType, fuelType,
+            transmission, price, mileage, year
+        })
+
+        if (xToken) {
+            const { payload } = protectSession(xToken as string);
+            if (payload.role !== "user") {
+                if (page && size) {
+                    const pageNumber = parseInt(page as string, 10);
+                    const pageSize = parseInt(size as string, 10);
+
+                    const paginatedData = paginate(filteredData, pageNumber, pageSize);
+                    res.status(200).json(paginatedData);
+                    return;
+                }
+                // no pagination
+                res.status(200).json(filteredData);
+                return;
+            }
+        }
+
+        if (page && size) {
+            const pageNumber = parseInt(page as string, 10);
+            const pageSize = parseInt(size as string, 10);
+
+            const paginatedData = paginate(formattedData, pageNumber, pageSize);
+            res.status(200).json(paginatedData);
             return;
         }
-        res.status(200).json([]);
+        // no pagination
+        res.status(200).json(formattedData);
         return;
     }
 }
