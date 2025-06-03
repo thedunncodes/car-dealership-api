ed66edad91ab4253d3c8b896981938ef4060f902
thedunncodes <tobioluwa348@gmail.com>
2025-06-03 02:18:17 +0100

    UPDATE: 'updateCar ' controller for car informations

--- src/controllers/CarsController.ts
+++ src/controllers/CarsController.ts
@@ -1,10 +1,13 @@
 import { DateTime } from "luxon";
-import { Request, Response } from "express";
+import { ObjectId } from "mongodb";
 import dbClient from "../libs/database/db";
-import { carsData } from "../constants/carTypes";
+import { Request, Response } from "express";
+import { carsData, carsUpdateData } from "../constants/carTypes";
 import protectSession from "../utils/authUtils/protectSession";
 import validateCarData from "../utils/inputValidations/validateCarsInputs";
-import { ObjectId } from "mongodb";
+import validateCarUpdate from "../utils/inputValidations/validateCarUpdate";
+import checkCarExists from "../utils/dbUtils/checkCarExists";
+import updateCarData from "../utils/dbUtils/updateCarData";
 
 export default class CarsController {
     static async createCar(req: Request, res: Response) {
@@ -62,6 +65,72 @@ export default class CarsController {
                 return;
             }
 
+        }
+
+        res.status(500).json({ error: "Internal server error, visit '/stat' endpoint." });
+        return;
+    }
+
+    static async updateCar(req: Request, res: Response) {
+        const {
+            brand, model, bodyType, transmission, price,
+            horsePower, fuelType,  mileage, year, imgUrl, sold
+        }: carsUpdateData = req.body || {};
+        const carId = req.params.carId; 
+        const xToken = req.headers['x-token'];
+                
+        if (!xToken) {
+            res.status(401).json({ error: "Unauthorized, no access token provided, procced to '/login' route " });
+            return;
+        }
+        const { validSession, payload } = protectSession(xToken as string);
+        if (validSession) {
+            if (payload.role === "user") {
+                res.status(403).json({ error: "Forbidden, access denied." });
+                return;
+            }
+        } else {
+            res.status(401).json({ error: "Invalid session token, or expired session" });
+            return;
+        }
+
+        const err = validateCarUpdate({
+            brand, model, bodyType, transmission, price,
+            horsePower, fuelType,  mileage, year, imgUrl, sold
+        })
+        
+        if (err.error) {
+            res.status(400).json({ error: err.error });
+            return;
+        }
+        
+        if (dbClient.isAlive() && payload.id) {
+            if (!carId) {
+                res.status(400).json({ error: "Car ID is required" });
+                return;
+            } else {
+                const { carPresent } = await checkCarExists(carId);
+                if (!carPresent) {
+                    res.status(404).json({ error: "Car not found" });
+                    return;
+                }
+            }
+            const result = await updateCarData(carId, payload.id, {
+                brand, model, bodyType, transmission, price,
+                horsePower, fuelType,  mileage, year, imgUrl, sold
+            })
+
+            if (result.updated) {
+                if (result.changes) {
+                    res.status(200).json({ message: `Car with id '${carId}' updated successfully` });
+                    return; 
+                }
+                res.status(200).json({ message: `No changes made` });
+                return; 
+            } else {
+                res.status(result.code? result.code : 400).json({ error: result.error });
+                return;
+            }
         }
 
         res.status(500).json({ error: "Internal server error, visit '/stat' endpoint." });
