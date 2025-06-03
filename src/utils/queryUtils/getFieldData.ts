92844f0a399bb055b21e61724f72531968f0d790
thedunncodes <tobioluwa348@gmail.com>
2025-06-03 09:08:54 +0100

    UPDATE: function to get all the data for the selected queries

---
+++ src/utils/queryUtils/getFieldData.ts
@@ -0,0 +1,49 @@
+import { WithId, Document } from "mongodb";
+import { carQueryData } from "../../constants/carTypes";
+import {
+    brandQuery, modelQuery, bodyTypeQuery, transmissionQuery,
+    fuelTypeQuery, yearQuery, priceQuery, mileageQuery
+} from "../dbUtils/carQueries";
+
+export default async function getFieldData(
+    validFields: (keyof carQueryData)[], queryFields: carQueryData) {
+    const queryData: WithId<Document>[] = [];
+
+    for (const field of validFields) {
+        if (field === "brand" && queryFields[field]) {
+            const carBrands = await brandQuery(queryFields[field]);
+            queryData.push(...carBrands);
+        }
+
+        if (field === "model" && queryFields[field]) {
+            const carModels = await modelQuery(queryFields[field]);
+            queryData.push(...carModels);
+        }
+        if (field === "bodyType" && queryFields[field]) {
+            const carBodyTypes = await bodyTypeQuery(queryFields[field]);
+            queryData.push(...carBodyTypes);
+        }
+        if (field === "transmission" && queryFields[field]) {
+            const carTransmissions = await transmissionQuery(queryFields[field]);
+            queryData.push(...carTransmissions);
+        }
+        if (field === "fuelType" && queryFields[field]) {
+            const carFuelTypes = await fuelTypeQuery(queryFields[field]);
+            queryData.push(...carFuelTypes);
+        }
+        if (field === "year" && queryFields[field]) {
+            const carYears = await yearQuery(Number(queryFields[field]));
+            queryData.push(...carYears);
+        }
+        if (field === "price" && queryFields[field]) {
+            const carPrices = await priceQuery(Number(queryFields[field]));
+            queryData.push(...carPrices);
+        }
+        if (field === "mileage" && queryFields[field]) {
+            const carMileages = await mileageQuery(Number(queryFields[field]));
+            queryData.push(...carMileages);
+        }
+    }
+
+    return queryData;
+}
