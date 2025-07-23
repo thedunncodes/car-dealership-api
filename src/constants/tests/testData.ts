3c42cff98c00626961e00b65f4c88d9deb91a600
thedunncodes <tobioluwa348@gmail.com>
2025-07-23 18:47:13 +0100

    Mock data for E2E tests

---
+++ src/constants/tests/testData.ts
@@ -0,0 +1,35 @@
+const mockData = [
+    {
+        name: "",
+        email: "",
+        password: ""
+    },
+    {
+        name: "Valid User",
+        email: "valid@email.com",
+        password: 12467
+    },
+    {
+        name: "Valid User",
+        email: "invalid-email",
+        password: "1234567"
+    },
+    {
+        name: "Valid User",
+        email: "valid@email.com",
+        password: "1234"
+    },
+    {
+        name: "Valid User",
+        email: "unregistered@email.com",
+        password: "1234567"
+    },
+
+    {
+        name: "Valid User",
+        email: "registered@email.com",
+        password: "1234567"
+    },
+]
+
+export default mockData;
