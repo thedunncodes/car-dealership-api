5cf8914bf13931ca1508cdd8b96b32691565f17b
thedunncodes <tobioluwa348@gmail.com>
2025-08-12 14:00:34 +0100

    UPDATE: E2E test for 'PUT /user/update' and 'DELETE /user/delete' endpoints

--- __tests__/controllers/UserController.test.ts
+++ __tests__/controllers/UserController.test.ts
@@ -41,7 +41,7 @@ describe("User Controller Tests", () => {
         });
     });
 
-    describe("GET /user", () => {
+    describe("Tests on protected routes", () => {
         let token: string = 'no-token';
         
         it("Should return 200 OK and a token on 'POST /login' with valid data", async () => {
@@ -57,17 +57,69 @@ describe("User Controller Tests", () => {
             token = loginRes.body.token;
         });
 
-        it("Should return user information on 'GET /user' with valid token", async () => {
-            const res = await request(app).get('/user').set('x-token', token);
-            const userData = res.body.myInfo;
-            expect(res.status).toEqual(200);
-            expect(userData).toHaveProperty('id');
-            expect(userData).toHaveProperty('name');
-            expect(userData).toHaveProperty('email');
-            expect(userData).toHaveProperty('role');
-            expect(userData.id).toBeDefined();
-            expect(userData.email).toEqual(mockData[5].email);
-            expect(['user', 'admin', 'staff']).toContain(userData.role);
-        })
+        describe("GET /user", () => {
+            it("Should return 401 Unauthorized on 'GET /user' with no token", async () => {
+                const res = await request(app).get('/user');
+                expect(res.status).toEqual(401);
+            });
+
+            it("Should return 401 Unauthorized on 'GET /user' with invalid token", async () => {
+                const res = await request(app).get('/user').set('x-token', `invalid-token`);
+                expect(res.status).toEqual(401);
+            })
+            
+            it("Should return user information on 'GET /user' with valid token", async () => {
+                const res = await request(app).get('/user').set('x-token', token);
+                const userData = res.body.myInfo;
+                expect(res.status).toEqual(200);
+                expect(userData).toHaveProperty('id');
+                expect(userData).toHaveProperty('name');
+                expect(userData).toHaveProperty('email');
+                expect(userData).toHaveProperty('role');
+                expect(userData.id).toBeDefined();
+                expect(userData.email).toEqual(mockData[5].email);
+                expect(['user', 'admin', 'staff']).toContain(userData.role);
+            })
+        });
+
+        describe("PUT /user/update", () => {
+            it("Should return 401 Unauthorized on 'PUT /user/update' with no token", async () => {
+                const res = await request(app).put('/user/update');
+                expect(res.status).toEqual(401);
+            });
+
+            it("Should return 401 Unauthorized on 'PUT /user/update' with invalid token", async () => {
+                const res = await request(app).put('/user/update').set('x-token', `invalid-token`);
+                expect(res.status).toEqual(401);
+            })
+
+            it("Should return 200 OK on 'PUT /user/update' with valid data", async () => {
+                const res = await request(app).put('/user/update').set('x-token', token).send({
+                    name: "User Updated"
+                })
+                console.log(res.body);
+                expect(res.status).toEqual(200);
+                expect(["User data updated successfully, Kindly login again to continue.",
+                    "User data updated successfully", "No changes made"
+                ]).toContain(res.body.message);
+            });
+        });
+
+        describe("DELETE /user/delete", () => {
+            it("Should return 401 Unauthorized on 'DELETE /user/delete' with no token", async () => {
+                const res = await request(app).delete('/user/delete');
+                expect(res.status).toEqual(401);
+            });
+
+            it("Should return 401 Unauthorized on 'DELETE /user/delete' with invalid token", async () => {
+                const res = await request(app).delete('/user/delete').set('x-token', `invalid-token`);
+                expect(res.status).toEqual(401);
+            })
+
+            it("Should return 200 OK on 'DELETE /user/delete' with valid token", async () => {
+                const res = await request(app).delete('/user/delete').set('x-token', token);
+                expect(res.status).toEqual(200);
+            });
+        });
     });
 });
