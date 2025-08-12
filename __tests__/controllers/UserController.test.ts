690eb404d4506214be02e24b15c6416ed6785c4a
thedunncodes <tobioluwa348@gmail.com>
2025-08-12 11:07:18 +0100

    UPDATE: E2E test for 'GET /user'

--- __tests__/controllers/UserController.test.ts
+++ __tests__/controllers/UserController.test.ts
@@ -3,7 +3,7 @@ import app from "../../src/server";
 import dbClient from "../../src/libs/database/db";
 import mockData from "../../src/constants/tests/testData";
 
-describe("Auth Controller Tests", () => {
+describe("User Controller Tests", () => {
     
     afterAll(async () => {
         if (dbClient.isAlive()) {
@@ -35,10 +35,39 @@ describe("Auth Controller Tests", () => {
             const res = await request(app).post('/register').send(mockData[5]);
             if (res.body.error) {
                 expect(res.body.error).toEqual(`User with email '${mockData[5].email}' already exists`);
-                console.log(res.body.error);
                 return;
             }
             expect(res.status).toEqual(201);
         });
+    });
+
+    describe("GET /user", () => {
+        let token: string = 'no-token';
+        
+        it("Should return 200 OK and a token on 'POST /login' with valid data", async () => {
+            const res = await request(app).post('/register').send(mockData[5]);
+            if (res.body.error) {
+                expect(res.body.error).toEqual(`User with email '${mockData[5].email}' already exists`);
+            } else {
+                expect(res.status).toEqual(201);
+            }
+            const loginRes = await request(app).post('/login').send(mockData[5]);
+            expect(loginRes.status).toEqual(200);
+            expect(loginRes.body.token).toBeDefined();
+            token = loginRes.body.token;
+        });
+
+        it("Should return user information on 'GET /user' with valid token", async () => {
+            const res = await request(app).get('/user').set('x-token', token);
+            const userData = res.body.myInfo;
+            expect(res.status).toEqual(200);
+            expect(userData).toHaveProperty('id');
+            expect(userData).toHaveProperty('name');
+            expect(userData).toHaveProperty('email');
+            expect(userData).toHaveProperty('role');
+            expect(userData.id).toBeDefined();
+            expect(userData.email).toEqual(mockData[5].email);
+            expect(['user', 'admin', 'staff']).toContain(userData.role);
+        })
     });
 });
