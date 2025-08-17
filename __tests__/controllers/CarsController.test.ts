1a3314acbe41ee591afd1244035412b007efca35
thedunncodes <tobioluwa348@gmail.com>
2025-08-17 12:27:04 +0100

    UPDATE: E2E test for 'PUT /inventory/cars/update/:carId' and 'POST /inventory/cars/buy/:carId' endpoints

--- __tests__/controllers/CarsController.test.ts
+++ __tests__/controllers/CarsController.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-// import { ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import app from "../../src/server";
 import dbClient from "../../src/libs/database/db";
 import mockData from "../../src/constants/tests/testData";
@@ -16,8 +16,11 @@ describe("Cars Controller Tests", () => {
         }
     });
 
+    const carData = { ...mockCarsData[7] };
+    carData.model = 'G-toBeCreatedAndDeleted';
     let adminToken: string = 'no-token';
     let userToken: string = 'no-token';
+    let carId: string = '';
 
 
     it("Should return 200 OK and 2 tokens on 'POST /login' with valid data for admin and normal user", async () => {
@@ -43,6 +46,23 @@ describe("Cars Controller Tests", () => {
         expect(userLoginRes.body.token).toBeDefined();
         adminToken = adminLoginRes.body.token;
         userToken = userLoginRes.body.token;
+    });
+
+    it("Should create a car, return 201 created and set carId", async () => {
+        const res = await request(app).post('/inventory/cars/create').send(carData).set('x-token', adminToken);
+        expect(res.status).toEqual(201);
+        expect(res.body.message).toEqual(`Car '${carData.brand} ${carData.model}' created and added to inventory succesfully`);
+
+        const url = `/cars?brand=${carData.brand.toLowerCase()}` +
+            `&model=${carData.model.toLowerCase()}` +
+            `&bodyType=${carData.bodyType.toLowerCase()}` +
+            `&fuelType=${carData.fuelType}` +
+            `&transmission=${carData.transmission}` +
+            `&price=${carData.price}` +
+            `&mileage=${carData.mileage}` +
+            `&year=${carData.year}`
+        const carRes = await request(app).get(url);
+        carId = carRes.body[0].id;
     });
 
 
@@ -103,5 +123,121 @@ describe("Cars Controller Tests", () => {
             expect(res.status).toEqual(201);
             expect(res.body.message).toEqual(`Car '${mockCarsData[7].brand} ${mockCarsData[7].model}' created and added to inventory succesfully`);
         });
+    });
+
+    describe("PUT /inventory/cars/update/:carId", () => {
+        it("Should return 401 Unauthorized on 'PUT /inventory/cars/update/:carId' with no token", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`);
+            expect(res.status).toEqual(401);
+        });
+
+        it("Should return 401 Unauthorized on 'PUT /inventory/cars/update/:carId' with invalid token", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).set('x-token', `invalid-token`);
+            expect(res.status).toEqual(401);
+        });
+        
+        it("Should return 403 forbidden on 'PUT /inventory/cars/update/:carId' with a valid user token", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).set('x-token', userToken);
+            expect(res.status).toEqual(403);
+            expect(res.body.error).toEqual("Forbidden, access denied.");
+        });
+
+        it("Should return 404 Not Found on 'PUT /inventory/cars/update/:carId' with a valid token and non-existing carId", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${new ObjectId()}`).send({ price: 3000 }).set('x-token', adminToken);
+            expect(res.status).toEqual(404);
+            expect(res.body.error).toEqual("Car not found");
+        });
+        
+        it("Should return 404 Not Found on 'PUT /inventory/cars/update/:carId' with a valid token and invalid carId", async () => {
+            const res = await request(app).put('/inventory/cars/update/60d5-invalid-id').send({ price: 3000 }).set('x-token', adminToken);
+            expect(res.status).toEqual(404);
+            expect(res.body.error).toEqual("Car not found");
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with empty data", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send({}).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with data that has an invalid 'bodyType' field", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[2]).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with data that has an invalid 'transmission' field", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[3]).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with data that has an invalid 'fuelType' field", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[4]).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with data that has an invalid 'year' field", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[5]).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 400 Bad Request on 'PUT /inventory/cars/update/:carId' with data that has an invalid 'imgUrl' field", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[6]).set('x-token', adminToken);
+            expect(res.status).toEqual(400);
+        });
+
+        it("Should return 200 OK on 'PUT /inventory/cars/update/:carId' with valid data", async () => {
+            const res = await request(app).put(`/inventory/cars/update/${carId}`).send(mockCarsData[7]).set('x-token', adminToken);
+            expect(res.status).toEqual(200);
+            expect(res.body.message).toEqual(`Car with id '${carId}' updated successfully`);
+        });
+    });
+
+    describe("POST /inventory/cars/buy/:carId", () => {
+        const amountPaid: number = 30 
+
+        it("Should return 401 Unauthorized on 'POST /inventory/cars/buy/:carId' with no token", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`);
+                expect(res.status).toEqual(401);
+            });
+
+            it("Should return 401 Unauthorized on 'POST /inventory/cars/buy/:carId' with invalid token", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`).set('x-token', `invalid-token`);
+                expect(res.status).toEqual(401);
+            })
+
+            it("Should return 404 Not Found on 'POST /inventory/cars/buy/:carId' with incorrect car Id", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${new ObjectId()}`).set('x-token', adminToken);
+                expect(res.status).toEqual(404);
+                expect(res.body.error).toEqual("Car not found/Invalid car id ");
+            });
+
+            it("Should return 400 Bad Request on 'POST /inventory/cars/buy/:carId' with invalid car Id", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/60d5-invalid-id`).set('x-token', adminToken);
+                expect(res.status).toEqual(404);
+                expect(res.body.error).toEqual("Car not found/Invalid car id ");
+            });
+
+            it("Should return 400 Bad Request on 'POST /inventory/cars/buy/:carId' with no amount paid", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`).set('x-token', adminToken);
+                expect(res.status).toEqual(400);
+                expect(res.body.error).toEqual("Invalid amount provided, it should be a positive number");
+            });
+
+            it("Should return 400 Bad Request on 'POST /inventory/cars/buy/:carId' with Insufficient amount paid", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`).send({ amountPaid }).set('x-token', adminToken);
+                expect(res.status).toEqual(400);
+                expect(res.body.error).toEqual(`Insufficient amount, the car price is $${carData.price}, but you provided $${amountPaid}`);
+            });
+
+            it("Should return 200 OK on 'POST /inventory/cars/buy/:carId' with valid carId and amount paid", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`).send({ amountPaid: carData.price }).set('x-token', adminToken);
+                expect(res.status).toEqual(200);
+                expect(res.body.message).toContain("Purchase successful with purchase id")
+            });
+
+            it("Should return 400 Bad Request on 'POST /inventory/cars/buy/:carId' with valid carId and amount paid, indicating the car is sold", async () => {
+                const res = await request(app).post(`/inventory/cars/buy/${carId}`).send({ amountPaid: carData.price }).set('x-token', adminToken);
+                expect(res.status).toEqual(400);
+                expect(res.body.error).toEqual(`Car with id '${carId}' already sold`)
+            });
     });
 });
