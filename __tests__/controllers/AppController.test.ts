845d6da90696ee1690bb44ee2bc58720356a3acf
thedunncodes <tobioluwa348@gmail.com>
2025-08-17 22:30:27 +0100

    UPDATE: E2E test for 'GET /inventory/cars/sales' endpoint

--- __tests__/controllers/AppController.test.ts
+++ __tests__/controllers/AppController.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "../../src/server";
 import dbClient from "../../src/libs/database/db";
+import mockData from "../../src/constants/tests/testData";
 
 
 describe("App Controller Tests", () => {
@@ -79,4 +80,60 @@ describe("App Controller Tests", () => {
             expect(resPgn.body[0]).toHaveProperty('imgUrl');
         });
     });
+
+    describe("GET /inventory/cars/sales", () => {
+        let normalToken: string = 'no-token';
+        let staffToken: string = 'no-token';
+        
+        it("Should return 200 OK and 2 tokens on 'POST /login' with valid data for a staff and a normal user", async () => {
+            // create a non-staff user
+            const res = await request(app).post('/register').send(mockData[5]);
+            if (res.body.error) {
+                expect(res.body.error).toEqual(`User with email '${mockData[5].email}' already exists`);
+            } else {
+                expect(res.status).toEqual(201);
+            }
+            const loginRes = await request(app).post('/login').send(mockData[5]);
+            expect(loginRes.status).toEqual(200);
+            expect(loginRes.body.token).toBeDefined();
+            normalToken = loginRes.body.token;
+
+            // Create a staff user
+            const staffRes = await request(app).post(`/admin/register/${process.env.ADMIN_SLUG}`).send(mockData[7]);
+            if (staffRes.body.error) {
+                expect(staffRes.body.error).toEqual(`User with email '${mockData[7].email}' already exists`);
+            } else {
+                expect(staffRes.status).toEqual(201);
+                expect(staffRes.body.message).toEqual(`Staff with email '${mockData[7].email}' created succesfully`);
+            }
+            const staffLoginRes = await request(app).post('/login').send(mockData[7]);
+
+            expect(staffLoginRes.status).toEqual(200);
+            expect(staffLoginRes.body.token).toBeDefined();
+            staffToken = staffLoginRes.body.token;
+        });
+
+        it("Should return 401 Unauthorized on 'GET /inventory/cars/sales' with no token", async () => {
+            const res = await request(app).get('/inventory/cars/sales');
+            expect(res.status).toEqual(401);
+        });
+
+        it("Should return 401 Unauthorized on 'GET /inventory/cars/sales' with invalid token", async () => {
+            const res = await request(app).get('/inventory/cars/sales').set('x-token', `invalid-token`);
+            expect(res.status).toEqual(401);
+        });
+
+        it("Should return 403 forbidden on 'GET /inventory/cars/sales' with a valid user token", async () => {
+            const res = await request(app).get('/inventory/cars/sales').set('x-token', normalToken);
+            expect(res.status).toEqual(403);
+            expect(res.body.error).toEqual("Forbidden, access denied.");
+        });
+
+        it("Should return 200 OK and an array of sales on 'GET /inventory/cars/sales' with valid staff user token", async () => {
+            const res = await request(app).get('/inventory/cars/sales').set('x-token', staffToken);
+            expect(res.status).toEqual(200);
+            expect(res.body.totalSales).toBeDefined();
+            expect(res.body.sales).toBeInstanceOf(Array);
+        });
+    });    
 })
